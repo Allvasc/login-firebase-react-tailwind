510fa0bcfa9deeaf1bab8c2d3b6c1b9935c96521
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-05-11 21:33:49 -0300

    Criação dos componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import SignUp from "./components/SignUp"
+import { Route, Routes } from 'react-router-dom'
+import SignIn from './components/SignIn'
+import Account from './components/Account'
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-    
+    <div className="">
+      <h1 className='text-3xl text-center font-bold'>
+        Firebase Auth & context
+      </h1>
+      <Routes>
+        <Route path='/' element={<SignIn />} />
+        <Route path='/signUp' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
     </div>
   );
 }
