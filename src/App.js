535bb2e2b149c9fd9f303d77fb49e7fde61822f2
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-05-13 19:31:55 -0300

    implementação do signup finalizada

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import SignUp from "./components/SignUp"
 import { Route, Routes } from 'react-router-dom'
 import SignIn from './components/SignIn'
 import Account from './components/Account'
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="">
+    <div>
       <h1 className='text-3xl text-center font-bold'>
         Firebase Auth & context
       </h1>
-      <Routes>
-        <Route path='/' element={<SignIn />} />
-        <Route path='/signUp' element={<SignUp />} />
-        <Route path='/account' element={<Account />} />
-      </Routes>
+
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<SignIn />} />
+          <Route path='/signUp' element={<SignUp />} />
+          <Route path='/account' element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
+
     </div>
   );
 }
